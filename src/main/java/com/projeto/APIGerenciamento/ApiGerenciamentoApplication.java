a9581f1518abc445a2e9befe4e706e0f220e77a1
ArturLLopes <arturllopes@gmail.com>
2026-05-11 11:10:35 -0300

    Start: Inicio do projeto de criação de sala e reservas

---
+++ src/main/java/com/projeto/APIGerenciamento/ApiGerenciamentoApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.APIGerenciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciamentoApplication.class, args);
+	}
+
+}
